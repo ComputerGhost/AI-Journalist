c344aa75e56f9381e4ab6ebccf75b0986f4e0042
nathan <nathan@comptechweb.com>
2020-08-20 15:30:33 -0500

    Just hacking together some initial code.

---
+++ AI Journalist/AI Journalist/Models/Media.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Journalist.Models
+{
+    struct Media
+    {
+        public string SourceUrl;
+        public string DisplayUrl;
+        public bool IsVideo;
+        public byte[] PreviewImage;
+    }
+}
---
+++ AI Journalist/AI Journalist/Models/Update.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Journalist.Models
+{
+    struct Update
+    {
+        public string SourceUrl;
+        public DateTime When;
+        public Account Author;
+        public Account[] OthersTagged;
+        public Media[] Medias;
+        public string Caption;
+    }
+}
